f0392803dcbddf24ad9775111b7144967e075121
Pedro Vaz <ppvaz03@gmail.com>
2022-01-12 18:00:58 -0300

    chore: fix test coverage inconsistencies

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
   transform: {
