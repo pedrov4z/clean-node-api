49090433493405cc4f3b32d96c89da68f32210eb
Pedro Vaz <ppvaz03@gmail.com>
2022-01-21 14:32:17 -0300

    feat: make server error return the original error stack

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -34,7 +34,7 @@ export class SignUpController implements Controller {
       })
       return ok(account)
     } catch (err) {
-      return serverError()
+      return serverError(err)
     }
   }
 }
