397c71e4834ac286ebe4e89cc1f11b1021ac88a0
Pedro Vaz <ppvaz03@gmail.com>
2022-03-09 14:55:38 -0300

    fix: expressMiddlewareAdapter using request body instead of headers

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -4,7 +4,7 @@ import { HttpRequest, Middleware } from '../../presentation/protocols'
 export const adaptMiddleware = (middleware: Middleware): RequestHandler => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const httpRequest: HttpRequest = {
-      body: req.body
+      headers: req.headers
     }
     const httpResponse = await middleware.handle(httpRequest)
     if (httpResponse.statusCode === 200) {
