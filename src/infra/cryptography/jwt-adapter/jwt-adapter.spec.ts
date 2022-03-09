8251470b733084c68a60eccf2945aa17223bdd94
Pedro Vaz <ppvaz03@gmail.com>
2022-03-09 14:50:11 -0300

    refactor: use proper string

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -59,7 +59,7 @@ describe('Jwt Adapter', () => {
       jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
         throw new Error()
       })
-      const promise = sut.decrypt('any_id')
+      const promise = sut.decrypt('any_token')
       await expect(promise).rejects.toThrow()
     })
   })
