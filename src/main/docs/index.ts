c46f3a740acb8050766b363a40f007cfa12e1010
Pedro Vaz <ppvaz03@gmail.com>
2022-04-17 16:15:31 -0300

    docs: add login api swagger docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,36 @@
+import { badRequest, notFound, serverError, unauthorized } from './components'
+import { loginPath } from './paths'
+import { accountSchema, errorSchema, loginParamsSchema } from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: "Mango's course API for programmer polls",
     version: '1.0.0'
+  },
+  license: {
+    name: 'GPL-3.0-or-later',
+    url: 'https://www.gnu.org/licenses/gpl-3.0.txt'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    error: errorSchema,
+    loginParams: loginParamsSchema
+  },
+  components: {
+    badRequest,
+    notFound,
+    serverError,
+    unauthorized
   }
 }
