96e90d4e93d1467ac649d028e81451c82aa1e7c2
Pedro Vaz <ppvaz03@gmail.com>
2022-05-19 16:24:27 -0300

    docs: add add-survey api docs

--- src/main/docs/paths/survey-path.ts
+++ src/main/docs/paths/survey-path.ts
@@ -26,5 +26,35 @@ export const surveyPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+  post: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Poll'],
+    summary: 'API endpoint for creating a poll',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/addSurveyParams'
+          }
+        }
+      }
+    },
+    responses: {
+      204: {
+        description: 'Successful creation'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }
